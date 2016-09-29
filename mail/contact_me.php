70f2534a787527b6a76316aa7b8e6c48668c1421
Mathieu Leblond <mathieu.leblond@gmail.com>
2016-09-28 21:36:45 -0400

    removed captcha

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -4,25 +4,25 @@ if(empty($_POST['name'])                   ||
    empty($_POST['email'])                  ||
    empty($_POST['phone'])                  ||
    empty($_POST['message'])                ||
-   empty($_POST['g-recaptcha-response'])   ||
+   //empty($_POST['g-recaptcha-response'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
      echo "No arguments Provided!";
      return false;
    }
    
-$captcha = $_POST['g-recaptcha-response'];
+//$captcha = $_POST['g-recaptcha-response'];
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-   
+/*   
 $response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LeCnwcUAAAAAPsZi73syU8Wl2adv5FBTH00ue7C&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
 if($response['success'] == false)
   {
     echo "No arguments Provided for captcha!";
     return false;
-  }
+  }*/
 // Create the email and send the message
 $to = 'mathieu.leblond@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
